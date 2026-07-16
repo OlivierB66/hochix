2faefa98aa0baf3c6815e230a59ddae55105a690
OlivierB66 <obottarlini@bouyguestelecom-solution.fr>
2026-07-16 17:23:00 +0200

    Create DM4CDY3.nix

---
+++ inventory/machines/DM4CDY3.nix
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2025 Ryan Lahfa <ryan.lahfa@numerique.gouv.fr>
+#
+# SPDX-License-Identifier: MIT
+
+{
+  securix.self.mainDisk = "/dev/nvme0n1";
+  securix.self.machine = {
+    hardwareSKU = "latitude5340";
+    serialNumber = "DM4CDY3";
+
+    users = [
+      "hochix"
+    ];
+  };
+}
